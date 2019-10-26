be964447e5f2b87851e64813bb744d48f0ae0b63
jrgavilanes <jrgavilanes@gmail.com>
2019-10-26 11:13:04 +0200

    Update program.py

--- rss_with_xml/program.py
+++ rss_with_xml/program.py
@@ -1,28 +1,13 @@
-from collections import namedtuple
-from xml.etree import ElementTree
-
-import requests
-
-Episode = namedtuple("Episode", "title link pubDate show_id")
-episode_data = {}
+import service
 
 
-def download_info():
-    url = "https://talkpython.fm/episodes/rss"
-    response = requests.get(url)
-    dom = ElementTree.fromstring(response.text)
-
-    items = dom.findall("channel/item")
-    count_episode = len(items)
-    for idx, item in enumerate(items):
-        episode = Episode(
-            item.find("title").text,
-            item.find("link").text,
-            item.find("pubDate").text,
-            count_episode - idx,
-        )
-        episode_data[episode.show_id] = episode
+def main():
+    print("Bienvenido a descargardor de TalkPython")
+    service.download_info()
+    for show_id in range(100, 130):
+        info = service.get_episodio(show_id)
+        print("{}: {}".format(info.title, info.link))
 
 
-def get_episodio(item_id: int) -> Episode:
-    return episode_data.get(item_id)
+if __name__ == '__main__':
+    main()
