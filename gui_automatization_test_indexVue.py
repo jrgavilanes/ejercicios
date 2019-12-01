fd9d63a9472ad0f2db8ac5a459740992b03730c9
jrgavilanes <jrgavilanes@gmail.com>
2019-12-01 09:57:13 +0100

    Update gui_automatization_test_indexVue.py

--- gui_automatization_test_indexVue.py
+++ gui_automatization_test_indexVue.py
@@ -51,19 +51,18 @@ def tabula():
     pyautogui.press("tab")
 
 
+def refresca():
+    pyautogui.press("f5")
+
+
 def enter():
     pyautogui.press("enter")
 
 
 def pantallazo(nombre_test):
-    directorio = os.path.join(ROOT_DIR, "tests", nombre_test)
+    directorio = os.path.join(ROOT_DIR, "tests")
 
     if not os.path.exists(directorio):
-        try:
-            os.makedirs(os.path.join(ROOT_DIR, "tests"))
-        except FileExistsError:
-            pass
-
         os.makedirs(directorio)
 
     fichero = now() + "_" + nombre_test.upper() + ".png"
@@ -74,11 +73,14 @@ def pantallazo(nombre_test):
 
 def test_login_ok():
 
+    refresca()
+
     if not (selecciona(311, 109) == "Control"):
         print("ERROR: Debe estar en la pantalla de login, maximizado y al 100%")
         exit(1)
 
-    click(920, 264)  # Me posiciono en el campo usuario
+    # Me posiciono en el campo usuario
+    click(920, 264)
     escribe("juanra")
     tabula()
     escribe("juanra")
@@ -86,7 +88,7 @@ def test_login_ok():
 
     espera()
 
-    if selecciona(1522, 162) == "Cerrar":
+    if selecciona(468, 414) == "dolores":
         print("test OK: login_ok")
     else:
         print("TEST KO: login_ok")
@@ -94,11 +96,149 @@ def test_login_ok():
     pantallazo("login_ok")
 
 
+def test_login_ko_clave_equivocada():
+
+    if not (selecciona(311, 109) == "Control"):
+        print("ERROR: Debe estar en la pantalla de login, maximizado y al 100%")
+        exit(1)
+
+    # Me posiciono en el campo usuario
+    click(920, 264)
+    escribe("juanra")
+    tabula()
+    escribe("clave_mala")
+    enter()
+
+    espera()
+
+    pantallazo("login_ko_clave_equivocada")
+
+    if selecciona(311, 109) == "Control":
+        print("test OK: login_ko_clave_equivocada")
+    else:
+        print("TEST KO: login_ko_clave_equivocada")
+
+    # Me posiciono en el campo usuario
+    click(920, 264)
+
+
+def test_crear_registro():
+
+    # Le doy al botón +
+    click(1850, 1011)
+    espera()
+    # voy al primer campo
+    click(373, 263)
+    escribe("titulo de algo")
+    tabula()
+    escribe("incidencia 12345")
+    tabula()
+    tabula()
+    escribe("nueva etiqueta")
+    # pincho primer tag
+    click(364, 631)
+    tabula()
+    escribe("relleno sintomas")
+    # pincho barra desplazamiento
+    click(1912, 1046)
+    # selecciono campo procedimiento
+    click(420, 710)
+    escribe("relleno procedimiento")
+    # pincho en guardar
+    click(1560, 1041)
+    pantallazo("crear_registro")
+    # pincho en volver atras
+    click(353, 1037)
+    espera()
+    # estoy en pantalla principal y pincho en buscador
+    click(370, 125)
+    escribe("algo")
+    espera()
+    pantallazo("buscador_encuentra")
+
+    if selecciona(435, 378) == "sintomas":
+        print("test OK: crear_registro")
+    else:
+        print("TEST KO: crear_registro")
+
+
+def test_editar_registro():
+
+    # Le doy al botón editar
+    click(1581, 267)
+    espera()
+    # voy al primer campo y le añado una "x" al final
+    click(643, 293)
+    escribe("x")
+    tabula()
+    # pincho barra desplazamiento
+    click(1912, 1046)
+    # pincho en guardar
+    click(1560, 1041)
+    pantallazo("editar_registro")
+    # pincho en volver atras
+    click(353, 1037)
+    espera()
+    # estoy en pantalla principal y pincho en buscador
+    click(370, 125)
+    escribe("algox")
+    espera()
+    pantallazo("buscador_encuentra")
+
+    if selecciona(435, 378) == "sintomas":
+        print("test OK: editar_registro")
+    else:
+        print("TEST KO: editar_registro")
+
+
+def test_borrar_registro():
+
+    # Le doy al botón editar
+    click(1581, 267)
+    espera()
+    # Le doy al botón borrar documento
+    click(1597, 245)
+    pantallazo("borrar_registro")
+    enter()
+    espera()
+    # estoy en pantalla principal y refresco datos
+    click(341, 70)
+    # pincho en buscador
+    click(370, 125)
+    escribe("algox")
+    espera()
+
+    if selecciona(573, 268) == "ningún":
+        print("test OK: borrar_registro")
+    else:
+        print("TEST KO: borrar_registro")
+
+    pantallazo("no_existe_registro")
+
+
+def test_cerrar_sesion():
+    # Le doy al botón editar
+    click(1565, 173)
+
+    espera()
+    pantallazo("cerrar_sesion")
+
+    if selecciona(311, 109) == "Control":
+        print("test OK: cerrar_sesion")
+    else:
+        print("TEST KO: cerrar_sesion")
+
+
 def main():
+    test_login_ko_clave_equivocada()
     test_login_ok()
+    test_crear_registro()
+    test_editar_registro()
+    test_borrar_registro()
+    test_cerrar_sesion()
 
-    root.destroy()  # objeto tk gui
-
+    # objeto tk gui
+    root.destroy()
     exit(0)
 
 
