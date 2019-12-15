81ad450f635b32b3f0127e7a41b5797712f31e5c
jrgavilanes <jrgavilanes@gmail.com>
2019-12-15 12:05:15 +0100

    Add files via upload

---
+++ ejemplo_flask_basico/README.md
@@ -0,0 +1,18 @@
+# Notas
+
+https://cs50.harvard.edu/web/notes/3/
+
+## Instalación
+
+$ pipenv install
+$ pipenv shell
+
+## Restaurar BD sqlite
+
+$ sqlite3 db.sqlite < db_backup.sql 
+
+
+## Arrancar applicación
+
+$ python application.py
+
---
+++ ejemplo_flask_basico/db_backup.sql
@@ -0,0 +1,28 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE passengers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR NOT NULL,
+    flight_id INTEGER
+);
+INSERT INTO passengers VALUES(1,'Alice',1);
+INSERT INTO passengers VALUES(2,'Bob',1);
+INSERT INTO passengers VALUES(3,'Charlie',2);
+INSERT INTO passengers VALUES(4,'Dave',2);
+INSERT INTO passengers VALUES(5,'Erin',4);
+INSERT INTO passengers VALUES(6,'Frank',6);
+INSERT INTO passengers VALUES(7,'Grace',6);
+INSERT INTO passengers VALUES(8,'juanra',1);
+CREATE TABLE flights (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    origin VARCHAR NOT NULL,
+    destination VARCHAR NOT NULL,
+    duration INTEGER NOT NULL
+);
+INSERT INTO flights VALUES(1,'New York','London',415);
+INSERT INTO flights VALUES(2,'Shanghai','Paris',760);
+INSERT INTO flights VALUES(3,'Istanbul','Tokyo',700);
+INSERT INTO flights VALUES(4,'New York','Paris',435);
+INSERT INTO flights VALUES(5,'Moscow','Paris',245);
+INSERT INTO flights VALUES(6,'Lima','New York',455);
+COMMIT;
