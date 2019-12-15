231116c8b3c1246b651508464a51c2a8816ae75d
jrgavilanes <jrgavilanes@gmail.com>
2019-12-15 17:44:20 +0100

    Update db_backup.sql

--- ejemplo_flask_basico/db_backup.sql
+++ ejemplo_flask_basico/db_backup.sql
@@ -1,9 +1,25 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
+
+CREATE TABLE flights (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    origin VARCHAR NOT NULL,
+    destination VARCHAR NOT NULL,
+    duration INTEGER NOT NULL
+);
+
+INSERT INTO flights VALUES(1,'New York','London',415);
+INSERT INTO flights VALUES(2,'Shanghai','Paris',760);
+INSERT INTO flights VALUES(3,'Istanbul','Tokyo',700);
+INSERT INTO flights VALUES(4,'New York','Paris',435);
+INSERT INTO flights VALUES(5,'Moscow','Paris',245);
+INSERT INTO flights VALUES(6,'Lima','New York',455);
+
 CREATE TABLE passengers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR NOT NULL,
-    flight_id INTEGER
+    flight_id INTEGER REFERENCES flights (id) ON DELETE CASCADE
+                                              ON UPDATE CASCADE
 );
 INSERT INTO passengers VALUES(1,'Alice',1);
 INSERT INTO passengers VALUES(2,'Bob',1);
@@ -13,16 +29,5 @@ INSERT INTO passengers VALUES(5,'Erin',4);
 INSERT INTO passengers VALUES(6,'Frank',6);
 INSERT INTO passengers VALUES(7,'Grace',6);
 INSERT INTO passengers VALUES(8,'juanra',1);
-CREATE TABLE flights (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    origin VARCHAR NOT NULL,
-    destination VARCHAR NOT NULL,
-    duration INTEGER NOT NULL
-);
-INSERT INTO flights VALUES(1,'New York','London',415);
-INSERT INTO flights VALUES(2,'Shanghai','Paris',760);
-INSERT INTO flights VALUES(3,'Istanbul','Tokyo',700);
-INSERT INTO flights VALUES(4,'New York','Paris',435);
-INSERT INTO flights VALUES(5,'Moscow','Paris',245);
-INSERT INTO flights VALUES(6,'Lima','New York',455);
+
 COMMIT;
