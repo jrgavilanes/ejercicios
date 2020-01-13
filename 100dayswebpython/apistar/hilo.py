6a4cb0b8c69835d44561ba5ff977418c45822057
jrgavilanes <jrgavilanes@gmail.com>
2020-01-13 20:04:00 +0100

    Add files via upload

---
+++ 100dayswebpython/apistar/hilo.py
@@ -0,0 +1,25 @@
+#!/home/janrax/Escritorio/api-start/venv/bin/python
+
+import threading
+import time
+
+def create_file():
+    x = 1
+    while True:
+        print("Secundario",x)
+        x+=1
+        time.sleep(1)
+
+hilo1=threading.Thread(target=create_file, daemon=True)
+hilo1.start()
+
+x = 1
+while True:
+    print("Primario",x)
+    x+=1
+    time.sleep(.5)
+
+hilo1.join()
+
+
+    
